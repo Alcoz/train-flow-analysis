eaa9d51079075b0e8f7d27e1841c88cae3070e39
Baptiste Darnala <darnala.b@gmail.com>
2026-07-30 18:48:27 +0200

    Transformation de la pipeline dbt pour qu'elle s'intègre avec le S3

--- scripts/data-pipeline.sh
+++ scripts/data-pipeline.sh
@@ -2,4 +2,12 @@
 
 uv run scripts/data-pipeline-bronze-to-silver.py
 
-# make dbt-run
+export $(cat .env | xargs)
+
+make dbt-run
+
+export AWS_ACCESS_KEY_ID="$MINIO_ACCESS_KEY"
+export AWS_SECRET_ACCESS_KEY="$MINIO_SECRET_ACCESS_KEY"
+export AWS_DEFAULT_REGION="$REGION_NAME"
+
+aws s3 cp warehouse/sncf_data_analysis.duckdb s3://sncf-bucket/data/gold/warehouse/sncf_data_analysis.duckdb --endpoint-url http://127.0.0.1:9000
